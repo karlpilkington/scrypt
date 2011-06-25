6d4d270975c7b93f94fad40bf1f9e41c190ed689
Allan Saddi <allan@saddi.com>
2011-06-25 16:25:10 -0700

    Collapse salsa20 module into scrypt.

--- scrypt.py
+++ scrypt.py
@@ -2,13 +2,62 @@ import hashlib
 
 from itertools import izip
 
-from salsa20 import salsa20core
 from pbkdf2 import PBKDF2
 
 
 __all__ = ['scrypt']
 
+MASK32 = 2**32-1
+
 BLOCK_WORDS = 16
+
+
+def rotl(n, r):
+    return ((n << r) & MASK32) | ((n & MASK32) >> (32 - r))
+
+
+def doubleround(x):
+    x[ 4] ^= rotl(x[ 0]+x[12], 7)
+    x[ 8] ^= rotl(x[ 4]+x[ 0], 9)
+    x[12] ^= rotl(x[ 8]+x[ 4],13)
+    x[ 0] ^= rotl(x[12]+x[ 8],18)
+    x[ 9] ^= rotl(x[ 5]+x[ 1], 7)
+    x[13] ^= rotl(x[ 9]+x[ 5], 9)
+    x[ 1] ^= rotl(x[13]+x[ 9],13)
+    x[ 5] ^= rotl(x[ 1]+x[13],18)
+    x[14] ^= rotl(x[10]+x[ 6], 7)
+    x[ 2] ^= rotl(x[14]+x[10], 9)
+    x[ 6] ^= rotl(x[ 2]+x[14],13)
+    x[10] ^= rotl(x[ 6]+x[ 2],18)
+    x[ 3] ^= rotl(x[15]+x[11], 7)
+    x[ 7] ^= rotl(x[ 3]+x[15], 9)
+    x[11] ^= rotl(x[ 7]+x[ 3],13)
+    x[15] ^= rotl(x[11]+x[ 7],18)
+    x[ 1] ^= rotl(x[ 0]+x[ 3], 7)
+    x[ 2] ^= rotl(x[ 1]+x[ 0], 9)
+    x[ 3] ^= rotl(x[ 2]+x[ 1],13)
+    x[ 0] ^= rotl(x[ 3]+x[ 2],18)
+    x[ 6] ^= rotl(x[ 5]+x[ 4], 7)
+    x[ 7] ^= rotl(x[ 6]+x[ 5], 9)
+    x[ 4] ^= rotl(x[ 7]+x[ 6],13)
+    x[ 5] ^= rotl(x[ 4]+x[ 7],18)
+    x[11] ^= rotl(x[10]+x[ 9], 7)
+    x[ 8] ^= rotl(x[11]+x[10], 9)
+    x[ 9] ^= rotl(x[ 8]+x[11],13)
+    x[10] ^= rotl(x[ 9]+x[ 8],18)
+    x[12] ^= rotl(x[15]+x[14], 7)
+    x[13] ^= rotl(x[12]+x[15], 9)
+    x[14] ^= rotl(x[13]+x[12],13)
+    x[15] ^= rotl(x[14]+x[13],18)
+
+
+def salsa20_8_core(x):
+    z = list(x)
+    for i in range(4):
+        doubleround(z)
+    for i in range(16):
+        z[i] = (z[i] + x[i]) & MASK32
+    return z
 
 
 def blockmix_salsa20_8(B, r=8):
@@ -20,12 +69,12 @@ def blockmix_salsa20_8(B, r=8):
     for i in range(0,2 * r * BLOCK_WORDS,2 * BLOCK_WORDS):
         for j in range(BLOCK_WORDS):
             T[j] ^= B[i + j]
-        Y[even:even+BLOCK_WORDS] = T = salsa20core(T, rounds=8)
+        Y[even:even+BLOCK_WORDS] = T = salsa20_8_core(T)
         even += BLOCK_WORDS
         
         for j in range(BLOCK_WORDS):
             T[j] ^= B[i + BLOCK_WORDS + j]
-        Y[odd:odd+BLOCK_WORDS] = T = salsa20core(T, rounds=8)
+        Y[odd:odd+BLOCK_WORDS] = T = salsa20_8_core(T)
         odd += BLOCK_WORDS
     return Y
 
@@ -61,7 +110,7 @@ def smix(B, N, r=8):
 
 
 def scrypt(password, salt, N, r, p, dkLen):
-    MFLen = 2 * r * 64
+    MFLen = 2 * r * 4 * BLOCK_WORDS
     t = PBKDF2(password, salt, 1, p * MFLen, digestmod=hashlib.sha256)
     B = []
     while t:
